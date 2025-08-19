3100cc5c25549326e3003eaa2c10e035db27f6e5
Lsdceo <lsilva41559@gmail.com>
2025-08-19 16:41:45 -0300

    Feat: criação da pagina sobre nós e atualização da pagina home e footer

---
+++ src/routes/index.js
@@ -0,0 +1,38 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import ProfileScreen from "../pages/Perfil";
+import Pets from "../pages/Pets/PetsProfile";
+import AddPet from "../pages/Pets/AddPets";
+import PetsDetails from "../pages/Pets/PetsDetails";
+import ConsulPending from "../pages/Consultations/ConsulPending";
+import ConsulCompleted from "../pages/Consultations/ConsulCompleted";
+import ConsulDetails from "../pages/Consultations/ConsulDetails";
+import ConsulCompleteDetails from "../pages/Consultations/ConsulCompletedDetails";
+import ScheduleAppointment from "../pages/Consultations/ScheduleAppointment";
+import ModalRegisterUser from "../components/ModalRegisterUser";
+import ModalRegisterVet from "../components/ModalRegisterVet";
+import AbaoutUs from "../pages/AboutUs";
+import App from "../pages/App";
+
+export default function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/perfil" element={<ProfileScreen />} />
+        <Route path="/pets" element={<Pets />} />
+        <Route path="/add-pet" element={<AddPet />} />
+        <Route path="/pets-details" element={<PetsDetails />} />
+        <Route path="/consultas" element={<ConsulPending />} />
+        <Route path="/consultas/concluidas" element={<ConsulCompleted />} />
+        <Route path="/detalhes-consulta" element={<ConsulDetails />} />
+        <Route path="/detalhes-consulta-concluida" element={<ConsulCompleteDetails />} />
+        <Route path="/agendar-consulta" element={<ScheduleAppointment />} />
+        <Route path="/register-user" element={<ModalRegisterUser />} />
+        <Route path="/register-vet" element={<ModalRegisterVet />} />
+        <Route path="/sobre-nos" element={<AbaoutUs />} />
+        <Route path="/app" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
